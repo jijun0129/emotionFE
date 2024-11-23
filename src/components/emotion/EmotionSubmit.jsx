f146f74900b000ebe1b6bc2004a781a0af555945
Injun Jeong <jijun897@naver.com>
2024-11-23 12:07:27 +0900

    feat: svg 파일 추기하여 submit 페이지 디자인 수정

--- src/components/emotion/EmotionSubmit.jsx
+++ src/components/emotion/EmotionSubmit.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
+import EmptyStress from '../../assets/emptyStress.svg';
+import FullStress from '../../assets/fullStress.svg';
+import Meh from '../../assets/meh.svg';
+import Frown from '../../assets/frown.svg';
+import Smile from '../../assets/smile.svg';
 
 const Form = styled.form`
 	width: 80%;
@@ -8,9 +14,20 @@ const Form = styled.form`
 	flex-direction: column;
 `;
 
+const Name = styled.div`
+	margin-top: 20px;
+	display: flex;
+	justify-content: center;
+`;
+const Content = styled.div`
+	margin-top: 10px;
+	display: flex;
+	justify-content: center;
+`;
+
 const Button = styled.button`
 	height: 35px;
-	margin-top: 10px;
+	margin: 20px;
 
 	font-size: 16px;
 	color: var(--color-white);
@@ -21,7 +38,30 @@ const Button = styled.button`
 	cursor: pointer;
 `;
 
+const HiddenRadio = styled.input`
+	display: none;
+`;
+const LabelEmotion = styled.label`
+	cursor: pointer;
+	margin: 10px;
+	img {
+		width: 70px;
+		height: 70px;
+		opacity: ${props => (props.checked ? 1 : 0.5)};
+	}
+`;
+
+const LabelStress = styled.label`
+	cursor: pointer;
+	margin: 10px;
+	img {
+		width: 50px;
+		height: 50px;
+	}
+`;
+
 const EmotionSubmit = () => {
+	const navigate = useNavigate();
 	const [emotion, setEmotion] = useState(null);
 	const [stress, setStress] = useState(null);
 
@@ -35,7 +75,7 @@ const EmotionSubmit = () => {
 	const handleSubmit = e => {
 		e.preventDefault();
 		if (emotion && stress) {
-			alert(`기분: ${emotion} 스트레스: ${stress}`);
+			navigate('/graph');
 		} else {
 			alert('기분과 스트레스를 입력해주세요.');
 		}
@@ -43,33 +83,64 @@ const EmotionSubmit = () => {
 
 	return (
 		<Form onSubmit={handleSubmit}>
-			<div>
-				{[1, 2, 3].map(star => (
-					<label key={star}>
-						<input
+			<Name>기분</Name>
+			<Content>
+				<HiddenRadio
+					type="radio"
+					value="1"
+					id="radio-1"
+					checked={emotion === '1'}
+					onChange={handleEmotionChange}
+				/>
+				<LabelEmotion htmlFor="radio-1" checked={emotion === '1'}>
+					<img src={Frown} alt="Frown" />
+				</LabelEmotion>
+				<HiddenRadio
+					type="radio"
+					value="2"
+					id="radio-2"
+					checked={emotion === '2'}
+					onChange={handleEmotionChange}
+				/>
+				<LabelEmotion htmlFor="radio-2" checked={emotion === '2'}>
+					<img src={Meh} alt="Meh" />
+				</LabelEmotion>
+				<HiddenRadio
+					type="radio"
+					value="3"
+					id="radio-3"
+					checked={emotion === '3'}
+					onChange={handleEmotionChange}
+				/>
+				<LabelEmotion htmlFor="radio-3" checked={emotion === '3'}>
+					<img src={Smile} alt="Smiley" />
+				</LabelEmotion>
+			</Content>
+			<Name>스트레스</Name>
+			<Content>
+				{[1, 2, 3, 4, 5].map(stressCount => (
+					<label key={stressCount}>
+						<HiddenRadio
 							type="radio"
-							value={star}
-							checked={emotion === star.toString()}
-							onChange={handleEmotionChange}
-						/>
-						{star} ★
-					</label>
-				))}
-			</div>
-			<div>
-				{[1, 2, 3, 4, 5].map(star => (
-					<label key={star}>
-						<input
-							type="radio"
-							value={star}
-							checked={stress === star.toString()}
+							value={stressCount}
+							id={`stress-${stressCount}`}
+							checked={stress === stressCount.toString()}
 							onChange={handleStressChange}
 						/>
-						{star} ★
+						<LabelStress
+							htmlFor={`stress-${stressCount}`}
+							checked={stress === stressCount.toString()}
+						>
+							{stress >= stressCount.toString() ? (
+								<img src={FullStress} alt={`FullStress ${stressCount}`} />
+							) : (
+								<img src={EmptyStress} alt={`EmptyStress ${stressCount}`} />
+							)}
+						</LabelStress>
 					</label>
 				))}
-			</div>
-			<Button type="submit">제출하기</Button>
+			</Content>
+			<Button type="submit">제출</Button>
 		</Form>
 	);
 };
